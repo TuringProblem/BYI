3ef6edbf41119783f535adf01881371e2d943f50
Override <122590935+TuringProblem@users.noreply.github.com>
2026-09-04 17:47:50 -0400

    feat: created basic card... going to fix thisx up alot

---
+++ sets/set.md
@@ -0,0 +1,9 @@
+so how I want to structure the page is 
+
+
+- definitions
+> - Smallest set
+> - Smallest set
+> - Equivalence relations
+> - Equivalence Classes 
+
--- sets/sets.js
+++ sets/sets.js
@@ -1,12 +1,35 @@
-(() => {
-  const button = document.getElementById('actionBtn');
-  const input = document.getElementById('inputData');
-  const dialog = document.getElementById('my-dialog');
+const button = document.getElementById('actionBtn');
+const input = document.getElementById('inputData');
+const dialog = document.getElementById('my-dialog');
 
-  button.addEventListener('click', () => {
-    dialog.innerHTML = `<p>${input.value}</p>
-      <button commandfor="my-dialog" command="close">Cancel</button>`;
-    dialog.showModal();
+button.addEventListener('click', () => {
+  dialog.innerHTML = `
+    <div class="flex flex-col gap-4 items-center fadder">
+      <p>${input.value}</p>
+      <button commandfor="my-dialog" command="close" class="bg-[#aaaaaa] gray text-white p-2 rounded-md cursor-pointer">Cancel</button>
+    </div>
+  `;
+  dialog.showModal();
+});
 
-  });
-})();
+// this is an example of how to create a custom card
+class SpecialCard extends HTMLElement {
+  constructor() {
+    super();
+    this.attachShadow({ mode: 'open' });
+  }
+  connectedCallback() { this.render(); }
+
+  render() {
+    const text = this.getAttribute('text');
+
+    this.shadowRoot.innerHTML = `
+      <div class="bg-blue-600 text-white p-4 rounded-xl shadow-md">
+        <h1 class="text-2xl font-bold text-gray-800 mb-2">Special Card</h1>
+        <p class="text-gray-600 mb-6">${text}.</p>
+      </div>
+    `;
+  }
+}
+
+customElements.define('special-card', SpecialCard);
